943ff2a443a308e1fc001df7129d4282da572422
ihgorek <aemen95@gmail.com>
2017-05-18 14:42:15 +0300

    lab10 fixed

--- lab10/prog/lab10_2.m
+++ lab10/prog/lab10_2.m
@@ -15,18 +15,18 @@ ylabel('I, A');
 xlabel('t,c');
 title('');
 ylim([-0.5,15]);
-xlim([0,3]);
+xlim([0,2.5]);
 
 hold on;
 plot(time_base,y_current2,'--');
 plot(time_base,y_current3,'-.');
 %plot(time_base,y_current4,':');
-plot(time_current,I1,'m:','LineWidth', 2);
-plot(time_current,I2,'m:','LineWidth', 2);
-plot(time_current,I3,'m:','LineWidth', 2);
+plot(time_current,I1,'black:','LineWidth', 2);
+plot(time_current,I2,'black:','LineWidth', 2);
+plot(time_current,I3,'black:','LineWidth', 2);
 %plot(time_current,I4,'m:','LineWidth', 2);
 
-legend('ip','0.25ip','1.75ip', 4);
+legend('Jm','0.5Jm','1.5Jm', 6);
 %%%%%√–¿‘»  — Œ–Œ—“»%%%
 subplot(2,2,3);
 plot(time_base,y_omega1);
@@ -34,18 +34,19 @@ grid on;
 ylabel('\omega, ‡‰/ÒÂÍ');
 xlabel('t,c');
 title('');
-ylim([0,12]);
-xlim([0,3]);
+ylim([0,11]);
+xlim([0,5]);
     
 hold on;
 plot(time_base,y_omega2,'--');
 plot(time_base,y_omega3,'-.');
 %plot(time_base,y_omega4,':');
-plot(time_current,omega1,'m:','LineWidth', 2);
-plot(time_current,omega2,'m:','LineWidth', 2);
-plot(time_current,omega3,'m:','LineWidth', 2);
+plot(time_current,omega1,'black:','LineWidth', 2);
+plot(time_current,omega2,'black:','LineWidth', 2);
+plot(time_current,omega3,'black:','LineWidth', 2);
 %plot(time_current,omega4,'m:','LineWidth', 2);
-legend('ip','0.25ip','1.75ip', 4);
+
+legend('Jm','0.5Jm','1.5Jm', 6);
 %%%%%%√–¿‘»  ”√À¿%%%%%%
 subplot(2,2,4);
 plot(time_base,y_alpha1);
@@ -53,11 +54,12 @@ grid on;
 ylabel('\alpha, ‡‰');
 xlabel('t,c');
 title('');
-ylim([0,3]);
-xlim([0,3]);
+ylim([0,0.3]);
+xlim([0,0.3]);
     
 hold on;
 plot(time_base,y_alpha2,'--');
 plot(time_base,y_alpha3,'-.');
 %plot(time_base,y_alpha4,':');
-legend('ip','0.25ip','1.75ip', 4);
+
+legend('Jm','0.5Jm','1.5Jm', 6);
