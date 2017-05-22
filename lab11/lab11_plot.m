49ddbc4d3b6b6bf979a77721a904252c5fbecad6
ihgorek <aemen95@gmail.com>
2017-05-22 19:33:53 +0300

    Lab11 vol.2

--- lab11/lab11_plot.m
+++ lab11/lab11_plot.m
@@ -1,36 +1,65 @@
 %%%%1
-subplot(2,2,1);
+%{
 plot(Up);
-xlabel('t,c');
-ylabel('U_p, B');
 ylim([0,11]);
-xlim([0,0.001]);
+xlim([0,0.004]);
 grid on;
 title('');
-%%%%2
-subplot(2,2,2);
-plot(F);
+hold on;
+plot(Up1,'--');
+plot(Up2,'.-');
+plot(Up3,':');
+xlabel('t,c');
+ylabel('Up, B')
+legend('Tu','2Tu','4Tu','6Tu', 8);
+ %}
+
+%%2
+%{
+ plot(F);
 xlabel('t,c');
 ylabel('F, H');
 ylim([-15,11]);
-xlim([0,0.006]);
+xlim([0,0.005]);
 grid on;
 title('');
-%%%%3
-subplot(2,2,3);
+hold on;
+plot(F1,'--');
+plot(F2,'.-');
+plot(F3,':');
+xlabel('t,c');
+ylabel('F, H');
+legend('Tu','2Tu','4Tu','6Tu', 8);
+%}
+%%3
+%{
+
 plot(V);
-xlabel('t,c');
-ylabel('V, ì/ñ');
-xlim([0,0.006]);
-ylim([-11,11]);
+xlim([0,0.005]);
+ylim([-11,12]);
 grid on;
 title('');
-%%%%4
-subplot(2,2,4);
+hold on;
+plot(V1,'--');
+plot(V2,'.-');
+plot(V3,':');
+xlabel('t,c');
+ylabel('V, m/c');
+legend('Tu','2Tu','4Tu','6Tu', 8);
+%}
+%%4
+%%{
+
 plot(x);
-xlabel('t,c');
-ylabel('x, ì');
 xlim([0,0.006]);
-ylim([-0.5,16]);
+ylim([-0.5,17]);
 grid on;
 title('');
+hold on;
+plot(x1,'--');
+plot(x2,'.-');
+plot(x3,':');
+xlabel('t,c');
+ylabel('x, m');
+legend('Tu','2Tu','4Tu','6Tu', 8);
+%}
